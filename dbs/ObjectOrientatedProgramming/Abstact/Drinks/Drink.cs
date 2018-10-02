7c0ae19f285c954f680e278dff7f3003276a5aee
petercoker <petercoker@protonmail.com>
2018-10-02 20:54:28 +0100

    update code

---
+++ dbs/ObjectOrientatedProgramming/Abstact/Drinks/Drink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class SoftDrink : Drink
+{
+    public string Colour { get; set; }
+
+    public SoftDrink()
+    {
+
+    }
+
+    public SoftDrink(string name, double price, string colour)
+        : base(name, price)
+    {
+        Colour = colour;
+    }
+
+    public override string ToString()
+    {
+        return "This is the soft drink class";
+    }
+
+    public override double GetTax()
+    {
+        return Price * .12;
+        //throw new NotImplementedException(); //This allow you to throw something without implemtentin somthing
+    }
+}
