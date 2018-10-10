507928576187a4926f04975de906185728ca0f52
Peter Coker <petercoker@protonmail.com>
2018-10-10 06:48:09 +0100

    created output class

--- dbs/ObjectOrientatedProgramming/PeterCokerCA1725266/OutputTeacherAndStudent.cs
+++ dbs/ObjectOrientatedProgramming/PeterCokerCA1725266/OutputTeacherAndStudent.cs
@@ -1,19 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using dbs.ObjectOrientatedProgramming.Inheritance;
-using dbs.ObjectOrientatedProgramming.IntroductionToClasses;
-using dbs.ObjectOrientatedProgramming.Inheritance.EmployeeExample;
-using dbs.ObjectOrientatedProgramming.Interface.PassengerExample;
-using dbs.ObjectOrientatedProgramming.Interface.TryFoodInterfaceExample;
-using dbs.ObjectOrientatedProgramming.PeterCokerCA1725266;
-using dbs.ObjectOrientatedProgramming.Interface.TryVehicleInterfaceExample;
-using System.Collections;
-using System.Collections.Generic;
-using dbs.ObjectOrientatedProgramming.IntroductionToClasses.BookExample;
 
-namespace dbs
+namespace dbs.ObjectOrientatedProgramming.PeterCokerCA1725266
 {
-    class Program
+    class OutputTeacherAndStudent
     {
         static void Main(string[] args)
         {
@@ -131,7 +121,5 @@ namespace dbs
 
             //}
 
-
         }
     }
-}
